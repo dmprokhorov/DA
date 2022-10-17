14885c27ce1bd5dd3b2f4ddf9c851f5b154ee92a
dmprokhorov <danila.prokhorov@gmail.com>
2022-10-17 16:40:13 +0300

    7

---
+++ 7/7.cpp
@@ -0,0 +1,58 @@
+﻿// 7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class TData {
+public:
+    unsigned long long sum;
+    unsigned short operation;
+};
+
+int main()
+{
+    unsigned n;
+    std::cin >> n;
+    std::vector<TData> data(n + 1);
+    data[1] = { 0, 0 };
+    for (unsigned i = 2; i < n + 1; ++i) {
+        data[i] = { data[i - 1].sum + i, 1 };
+        if ((!(i % 2)) && (data[i / 2].sum + i < data[i].sum)) {
+            data[i] = { data[i / 2].sum + i, 2 };
+        }
+        if ((!(i % 3)) && (data[i / 3].sum + i < data[i].sum)) {
+            data[i] = { data[i / 3].sum + i, 3 };
+        }
+    }
+    std::queue<unsigned short> way;
+    unsigned current = n;
+    while (current > 1) {
+        unsigned short operation = data[current].operation;
+        way.emplace(operation);
+        if (operation == 1) {
+            --current;
+        }
+        else {
+            current /= operation;
+        }
+    }
+    std::cout << data[n].sum << "\n";
+    while (way.size()) {
+        unsigned short operation = way.front();
+        if (operation == 1) {
+            std::cout << "-";
+        }
+        else {
+            std::cout << "/";
+        }
+        std::cout << operation << " ";
+        way.pop();
+    }
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
+
+
---
+++ 8/8.cpp
@@ -0,0 +1,74 @@
+﻿// 8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class TData {
+public:
+	std::vector<double> c;
+	unsigned price;
+	unsigned index;
+};
+
+unsigned N, M;
+std::vector<TData> matrix;
+
+std::pair<bool, std::vector<unsigned>> Array() {
+	std::vector<unsigned> result;
+	for (unsigned i = 0; i < N; ++i) {
+		int index = -1;
+		double min = 51.;
+		for (unsigned j = i; j < M; ++j) {
+			if ((matrix[j].c[i]) && (matrix[j].price < min)) {
+				index = j;
+				min = matrix[j].price;
+			}
+		}
+		if (index == -1) {
+			result.clear();
+			return { false, result };
+		}
+		std::swap(matrix[i], matrix[index]);
+		result.push_back(matrix[i].index);
+		for (unsigned j = i + 1; j < M; ++j) {
+			double coefficient = matrix[j].c[i] / matrix[i].c[i];
+			matrix[j].c[i] = 0.;
+			for (unsigned k = i + 1; k < N; ++k) {
+				matrix[j].c[k] -= coefficient * matrix[i].c[k];
+			}
+		}
+	}
+	return { true, result };
+}
+
+int main() {
+	std::cin >> M >> N;
+	matrix.resize(M);
+	for (unsigned i = 0; i < M; ++i) {
+		matrix[i].c = std::vector<double>(N);
+		for (unsigned j = 0; j < N; ++j) {
+			std::cin >> matrix[i].c[j];
+		}
+		std::cin >> matrix[i].price;
+		matrix[i].index = i + 1;
+	}
+	std::pair<bool, std::vector<unsigned>> data = Array();
+	bool possible = data.first;
+	std::vector<unsigned> result = data.second;
+	std::sort(result.begin(), result.end());
+	unsigned size = result.size();
+	if (!possible) {
+		std::cout << "-1";
+	}
+	for (unsigned i = 0; i < size; ++i) {
+		std::cout << result[i] << " ";
+	}
+	std::cout << "\n";
+	system("pause");
+	return 0;
+}
+
+
